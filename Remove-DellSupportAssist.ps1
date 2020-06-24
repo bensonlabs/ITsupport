262417fddae60e1f2d0b18290529ecea1a19c733
Justin Benson <jbenson.dev@gmail.com>
2020-06-24 11:32:18 -0700

    Update

--- Remove-DellSupportAssist.ps1
+++ Remove-DellSupportAssist.ps1
@@ -1,7 +1,7 @@
 #Justin Benson 6/24/20
 
 #Remove Dell SupportAssist msi 
-Uninstall-Package -ProviderName msi -Name 'Dell SupportAssist' -Verbose -WhatIf
+Uninstall-Package -ProviderName msi -Name 'Dell SupportAssist'
 
 #Remove Dell SupportAssist Windows Store app
-Get-AppxPackage *SupportAssist* | Remove-AppPackage -Verbose -WhatIf
+Get-AppxPackage *SupportAssist* | Remove-AppPackage
